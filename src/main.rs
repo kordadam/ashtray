89ef60e8e71d98ef2cd6850833716e856383486a
kordadam <83919951+kordadam@users.noreply.github.com>
2023-01-17 18:49:20 +0100

    Ash crate integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{
+    error::Error,
+    result::Result
+};
+
+struct VulkanApp;
+
+impl VulkanApp {
+    fn new() -> Result<Self, Box<dyn Error>> {
+        log::info!("Creating application.");
+        Ok(Self)
+    }
+
+    fn run(&mut self) {
+        log::info!("Running application.");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::Builder::new()
+        .filter(None, log::LevelFilter::max())
+        .init();
+
+    match VulkanApp::new() {
+        Ok(mut app) => app.run(),
+        Err(error) => log::error!("Failed to create application. Cause: {}", error)
+    }
 }
